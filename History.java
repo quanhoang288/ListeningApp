99bb3db1ed6de26dbb7d3df0f6539bc0588aaa91
nguyenminh09072000 <65122860+nguyenminh09072000@users.noreply.github.com>
2020-05-17 15:56:02 +0700

    Create History.java

---
+++ History.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.ArrayList;
+
+
+public class HistoryModel {
+    private ArrayList<History> historyList;
+    
+    public HistoryModel()
+    {
+       
+        historyList = getAllHistory();
+    }
+    public ArrayList<History> getAllHistory()
+    {
+        return new ArrayList<>();
+    }
+    }
