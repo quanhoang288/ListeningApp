8d50a790a17c73c87338603173380d35e9b4b867
nguyenminh09072000 <65122860+nguyenminh09072000@users.noreply.github.com>
2020-05-20 20:48:53 +0700

    fix history

--- History.java
+++ History.java
@@ -1,18 +1,55 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 
-public class HistoryModel {
-    private ArrayList<History> historyList;
+public class History {
+    private String topic;
+    private Date time;
+    private int score;
+    private int level;
+
+    }
+    public History(String topuc, Date time, int score, int level) {
+        this.topic = topic;
+        this.time = time;
+        this.score = 0;
+        this.level = level;
+    }
+
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+     public int getscore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
     
-    public HistoryModel()
-    {
-       
-        historyList = getAllHistory();
-    }
-    public ArrayList<History> getAllHistory()
-    {
-        return new ArrayList<>();
-    }
-    }
+    
+    
+}
